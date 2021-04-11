ee1fd18574fd7a42e186d0ffc2cdbbe1b500fc14
Vlad <hrrrrustic@gmail.com>
2021-04-11 04:12:29 +0300

    Немного прибрался в фиксере

--- NullCheckRemover/NullCheckRemover.CodeFixes/NullCheckRemoverCodeFixProvider.cs
+++ NullCheckRemover/NullCheckRemover.CodeFixes/NullCheckRemoverCodeFixProvider.cs
@@ -1,15 +1,9 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,16 +15,9 @@ namespace NullCheckRemover
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(NullCheckRemoverCodeFixProvider)), Shared]
     public class NullCheckRemoverCodeFixProvider : CodeFixProvider
     {
-        public sealed override ImmutableArray<string> FixableDiagnosticIds
-        {
-            get { return ImmutableArray.Create(NullCheckRemoverAnalyzer.DiagnosticId); }
-        }
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(NullCheckRemoverAnalyzer.DiagnosticId);
 
-        public sealed override FixAllProvider GetFixAllProvider()
-        {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
-            return WellKnownFixAllProviders.BatchFixer;
-        }
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
@@ -64,50 +51,6 @@ namespace NullCheckRemover
                 ConditionalAccessExpressionSyntax conditionalAccess => fixer.Fix(conditionalAccess),
                 AssignmentExpressionSyntax assignmentExpressionSyntax => fixer.Fix(assignmentExpressionSyntax)
             };
-
-            if (nodeForFix is BinaryExpressionSyntax eq)
-            {
-                return fixer.Fix(eq);
-            }
-            else if (nodeForFix is IsPatternExpressionSyntax patter)
-            {
-                IsPatternFix(patter, editor);
-            }
-            else if (nodeForFix is SwitchExpressionArmSyntax arm)
-            {
-                SwitchFix(arm, editor);
-            }
-            else if (nodeForFix is CaseSwitchLabelSyntax label)
-            {
-                return fixer.Fix(label);
-            }
-
-            return editor.GetChangedDocument();
-        }
-
-        private void IsPatternFix(IsPatternExpressionSyntax pattern, DocumentEditor editor)
-        {
-            if (pattern.Pattern is ConstantPatternSyntax constant)
-            {
-                var trueExpression = SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression);
-                editor.ReplaceNode(pattern, trueExpression);
-            }
-            else if (pattern.Pattern is UnaryPatternSyntax unary)
-            {
-                var falseExpression = SyntaxFactory.LiteralExpression(SyntaxKind.FalseLiteralExpression);
-                editor.ReplaceNode(pattern, falseExpression);
-            }
-            else if(pattern.Pattern is RecursivePatternSyntax recursive)
-            {
-                var trueExpression = SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression);
-                editor.ReplaceNode(pattern, trueExpression);
-            }
-        }
-
-        private Document SwitchFix(SwitchExpressionArmSyntax arm, DocumentEditor editor)
-        {
-            editor.RemoveNode(arm);
-            return editor.GetChangedDocument();
         }
     }
 }
--- NullCheckRemover/NullCheckRemover.CodeFixes/NullFixer/SyntaxNullFixer.BinaryExpression.cs
+++ NullCheckRemover/NullCheckRemover.CodeFixes/NullFixer/SyntaxNullFixer.BinaryExpression.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Editing;
 
 namespace NullCheckRemover.NullFixer
 {
